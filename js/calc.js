30d961bfb454f928fd6ad38ca0f20507ae5d095d
a.tishchenko <a.tishchenko@outlook.com>
2018-05-23 21:17:12 +0300

    Create start_page html. Started creating .css

--- js/calc.js
+++ js/calc.js
@@ -1,3 +1,5 @@
+/* global _*/
+
 let calculator = (function () {
     return {
         add: function (x, y) {
@@ -6,3 +8,19 @@ let calculator = (function () {
     };
 
 })();
+
+window.catalog.sort(compare);
+console.log(window.catalog);
+
+function compare(a, b) {
+    let aDate = new Date(a.dateAdded);
+    let bDate = new Date(b.dateAdded);
+
+    return - (aDate - bDate);
+}
+
+let tmpl = document.getElementById('ul-templ').innerHTML.trim();
+tmpl = _.template(tmpl);
+document.getElementById('holder').innerHTML = tmpl({
+    items: window.catalog
+});
--- script-compiled.js
+++ script-compiled.js
@@ -1,4 +1,6 @@
-"use strict";
+'use strict';
+
+/* global _*/
 
 var calculator = function () {
     return {
@@ -7,6 +9,22 @@ var calculator = function () {
         }
     };
 }();
+
+window.catalog.sort(compare);
+console.log(window.catalog);
+
+function compare(a, b) {
+    var aDate = new Date(a.dateAdded);
+    var bDate = new Date(b.dateAdded);
+
+    return -(aDate - bDate);
+}
+
+var tmpl = document.getElementById('ul-templ').innerHTML.trim();
+tmpl = _.template(tmpl);
+document.getElementById('holder').innerHTML = tmpl({
+    items: window.catalog
+});
 "use strict";
 
 /*global calculator*/
