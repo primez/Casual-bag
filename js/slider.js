dbabf4177ee619b019cf6e2ed750d4395931a825
a.tishchenko <a.tishchenko@outlook.com>
2018-05-28 23:41:49 +0300

    Finish styling catalog page

--- js/slider.js
+++ js/slider.js
@@ -1,6 +1,6 @@
 (function () {
     let sliderContainer = document.querySelector('.slider-container');
-    let firstSlide = sliderContainer.querySelector('.slides div:nth-child(1)');
+    let firstSlide = sliderContainer.querySelector('#first-slide');
     let secondSlide = sliderContainer.querySelector('.slides div:nth-child(2)');
     let thirdSlide = sliderContainer.querySelector('.slides div:nth-child(3)');
 
@@ -8,18 +8,21 @@
     let secondSlideBtn = sliderContainer.querySelector('#second-slide');
     let thirdSlideBtn = sliderContainer.querySelector('#third-slide');
 
-    firstSlideBtn.onclick = function () {
-        firstSlide.hidden = false;
+    function showCurrentSlide() {
+        firstSlide.hidden = true;
         secondSlide.hidden = true;
         thirdSlide.hidden = true;
-    };
-    
+        this.hidden = false;
+    }
+
+    firstSlideBtn.onclick = showCurrentSlide;
+
     secondSlideBtn.onclick = function () {
         firstSlide.hidden = true;
         secondSlide.hidden = false;
         thirdSlide.hidden = true;
     };
-    
+
     thirdSlideBtn.onclick = function () {
         firstSlide.hidden = true;
         secondSlide.hidden = true;
