5685a7f08b0f49d20a5d27bfdc67b4c9d1b3f8e3
a.tishchenko <a.tishchenko@outlook.com>
2018-05-31 21:11:37 +0300

    Improved slider

--- js/slider.js
+++ js/slider.js
@@ -8,6 +8,12 @@
     let secondSlideBtn = sliderContainer.querySelector('#second-slide-btn');
     let thirdSlideBtn = sliderContainer.querySelector('#third-slide-btn');
 
+    firstSlideBtn.onclick = showCurrentSlide;
+
+    secondSlideBtn.onclick = showCurrentSlide;
+
+    thirdSlideBtn.onclick = showCurrentSlide;
+
     function showCurrentSlide() {
         firstSlide.hidden = true;
         secondSlide.hidden = true;
@@ -16,14 +22,35 @@
         let slideId = this.dataset.id;
         let slide = sliderContainer.querySelector('#' + slideId);
         slide.hidden = false;
+    } 
+})();
+
+(function () {
+    let sliderContainer = document.querySelector('.slider-container');
+    let arrowLeft = sliderContainer.querySelector('.left-arrow');
+    let arrowRight = sliderContainer.querySelector('.right-arrow');
+    let slides = sliderContainer.querySelectorAll('.slides div');
+
+    arrowLeft.onclick = changeSlide(-1);
+    arrowRight.onclick = changeSlide(1);
+
+    let slideIndex = 1; 
+
+    function changeSlide(n) {
+        showSlides(slideIndex += n)
     }
 
-    firstSlideBtn.onclick = showCurrentSlide;
-
-    secondSlideBtn.onclick = showCurrentSlide;
-
-    thirdSlideBtn.onclick = showCurrentSlide;
+    function showSlides(n) {
+        
+        if (n > slides.length) {slideIndex = 1;}
+        if (n < 1) {slideIndex = slides.length;}
+        for (let i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        slides[slideIndex-1].style.display = "block";
+    } 
 })();
+
 
 
 
